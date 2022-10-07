148765953ed28c8b4f45112244563511acd28f0b
Ranger-HK <maxravindu99@gmail.com>
2022-10-08 00:08:17 +0530

    Updated All Ui

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
+import { View, Text } from 'react-native'
 import React from 'react'
 import Login from './screens/Login'
+import Register from './screens/Register';
+import Home from './screens/Home';
+import AddNewVehicleInfo from './screens/AddNewVehicleInfo';
+import VehicleInfo from './screens/VehicleInfo';
+import Update from './screens/Update';
+
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+
+
+
+const Stack = createStackNavigator();
+
+
 
 export default function App() {
   return (
-   
-    <Login/>
-   
+    <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="AddNewVehicleInfo" component={AddNewVehicleInfo} />
+        <Stack.Screen name="VehicleInfo" component={VehicleInfo} />
+        <Stack.Screen name="Update" component={Update} />
+      
+    </Stack.Navigator>
+    </NavigationContainer>
+  
   )
 }
--- screens/Login.js
+++ screens/Login.js
@@ -1,58 +1,57 @@
-import { View } from 'react-native'
+import { View,StyleSheet } from 'react-native'
 import React from 'react'
+import {NativeBaseProvider ,Text,VStack,Input,Button,Link,Divider,Image}from 'native-base'
 
-import {NativeBaseProvider,Text,VStack,Input,Button,Link,HStack,Avatar,Center,Image}from 'native-base'
-
-export default function Loging_Page() {
+export default function Login({navigation}) {
   return (
+    <View style={styles.container}>
     <NativeBaseProvider>
-
-        <VStack space={4} alignItems="center" mt="5%">
-        <Text bold fontSize="7xl" color="purple.700">Welcome Back</Text>
-        </VStack>
-
-            <Center>
-            <Image size="350px"
-            Thumbnail source= {require('../images/Login.png')} alt="Login" />
-            </Center>
-
-        <VStack space={4} alignItems="center" >
-        <Input variant="filled"  size="lg" w="75%" mx="auto" placeholder="Username" />
-        <Input type="password" size="lg" w="75%" mx="auto" variant="filled" placeholder="Password" /> 
-        <Button w="50%" mx="auto" fontSize="lg" alignItems="center" h="13%" variant="solid" size="xsm" onPress={() => console.log("hello world")}>Login</Button>
+      
+        <VStack space={4} alignItems="center" mt="20%">
+        <Text bottom={50} bold fontSize="6xl" color="#2980b9">Welcome Back</Text>
         
 
-        <HStack space={4} justifyContent="center">
-        <Link href="https://github.com/Ranger-HK">
-        <Avatar  bg="white" size="sm"  Thumbnail source= {require('../images/githubIcon.png') }  >  
-        </Avatar>
-        </Link>
+        <Text top="95px" marginRight="70%" bold fontSize="sm" color="indigo.400">Email</Text>
+        <Input  mt="20%" w="80%" variant="underlined" placeholder="Enter Email" />
+        <Text top="20px" marginRight="65%" bold fontSize="sm" color="indigo.400">Password</Text>
+        <Input w="80%" variant="underlined" placeholder="Enter Password" />
+        <Button mt="5%" w="50%" h="10%" backgroundColor="indigo.600" borderRadius="20" size="sm"onPress={()=>{navigation.navigate("Home")}}>Login</Button>
+        
+           </VStack>
+           <Text top="4px" left="77px" bold fontSize="xs">Don't have an account ?</Text>
+          
+           <Link href="" bottom="16px" left="218px" bold fontSize="xs" sExternal _text={{
+        color: "indigo.600"}} onPress={()=>{navigation.navigate("Register")}}>
+           Register Now!
+      </Link>
+      
 
-        <Link href="https://www.google.com/">
-        <Avatar bg="white" size="sm"  Thumbnail source= {require('../images/GoogleIcon.png') }  >  
-        </Avatar>
-        </Link>
+      <Divider mt="10%" left="40px" w="80px" my="2" _light={{
+        bg: "muted.800"
+      }} _dark={{
+        bg: "muted.50"
+      }} />
+      <Text bottom="19px" left="150px"  fontSize="sm">Or continue with</Text>
 
+      <Divider bottom="35px" left="280px" w="80px" my="2" _light={{
+        bg: "muted.800"
+      }} _dark={{
+        bg: "muted.50"
+      }} />
 
-        <Link href="https://www.facebook.com/ravindu.0">
-        <Avatar bg="white" size="sm"  Thumbnail source= {require('../images/FacebookIcon.png') }  >  
-        </Avatar>
-        </Link>
-        </HStack>
+            <Image  left="90px" bottom="12px" size="50px"Thumbnail source= {require('./assets/img/google-logo.png')} alt="car logo" />
+            <Image  left="170px" bottom="59px" size="40px"Thumbnail source= {require('./assets/img/Facebook-logo.png')} alt="car logo" />
+            <Image  left="250px" bottom="106px" size="46px"Thumbnail source= {require('./assets/img/apple-logo.png')} alt="car logo" />
+            <Image  left="250px" bottom="106px" size="46px"Thumbnail source= {require('./assets/img/Login.png')} alt="car logo" />
 
-    
-        <HStack space={3} justifyContent="center">
-        <Text  bold>You Don't Have An Account ? </Text>
-        <Link href="https://github.com/Ranger-HK" isExternal _text={{color: "blue.600"}}>Register Please</Link>  
-        </HStack>
-
-        </VStack>
-
-        <VStack space={3} alignItems="center" mt="10%">
-        <Text fontSize="xs"  bold>Designed By : Ravindu Prathibha </Text>
-        </VStack>
-       
     </NativeBaseProvider>
+    </View>
   )
+
 }
 
+const styles =StyleSheet.create({
+  container:{
+    flex: 1,
+  }
+})
