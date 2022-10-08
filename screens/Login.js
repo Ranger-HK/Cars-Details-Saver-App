de565ce3d9a986681fdb2587e8bffe5a3f94527d
Ranger-HK <maxravindu99@gmail.com>
2022-10-08 13:57:59 +0530

    Registerform Updated

--- screens/Login.js
+++ screens/Login.js
@@ -13,9 +13,9 @@ export default function Login({navigation}) {
         <Text  bottom={10} bold fontSize="6xl" color="#16a085" underline >Welcome Back</Text>
         
 
-        <Text top="100px" marginRight="83%" bold fontSize="sm" color="#079992">Email</Text>
+        <Text top="90px" marginRight="83%" bold fontSize="sm" color="#079992">Email</Text>
         <Input  mt="20%" h="40px" w="90%" variant="underlined"  placeholder="Enter Email" />
-        <Text top="30px" marginRight="77%" bold fontSize="sm" color="#079992">Password</Text>
+        <Text top="20px" marginRight="77%" bold fontSize="sm" color="#079992">Password</Text>
         <Input w="90%" h="40px" variant="underlined" placeholder="Enter Password" />
         <Button mt="5%" w="70%" h="10%" backgroundColor="#16a085"  borderRadius="10" size="lg"onPress={()=>{navigation.navigate("Home")}}>LOGIN</Button>
         
@@ -28,14 +28,14 @@ export default function Login({navigation}) {
       </Link>
      
 
-      <Divider mt="10%" left="20px" w="80px"  top="5px"  h="2px" my="2" _light={{
+      <Divider mt="10%" left="20px" w="80px"  top="4px"  h="2px" my="2" _light={{
         bg: "muted.800"
       }} _dark={{
         bg: "muted.50"
       }} />
       <Text bottom="19px" left="125px" color={"#1e272e"} fontSize="lg">Or continue with</Text>
 
-      <Divider bottom="35px" left="260px" h="2px" w="80px" my="2" _light={{
+      <Divider bottom="36px" left="260px" h="2px" w="80px" my="2" _light={{
         bg: "muted.800"
       }} _dark={{
         bg: "muted.50"
@@ -43,7 +43,7 @@ export default function Login({navigation}) {
 
 <Link href='https://www.google.com/'> <Image  left="75px" bottom="10px" size="50px"Thumbnail source= {require('./assets/img/google-logo.png')} alt="car logo" /></Link>
 <Link href='www.facebook.com'> <Image  left="160px" bottom="56px" size="40px"Thumbnail source= {require('./assets/img/Facebook-logo.png')} alt="car logo" /></Link>
-<Link href='https://www.apple.com/'>  <Image  left="235px" bottom="102px" size="46px"Thumbnail source= {require('./assets/img/apple-logo.png')} alt="car logo" /></Link>
+<Link href='https://www.apple.com/'>  <Image  left="231px" bottom="100px" size="46px"Thumbnail source= {require('./assets/img/apple-logo.png')} alt="car logo" /></Link>
 
           
            
--- screens/Register.js
+++ screens/Register.js
@@ -7,38 +7,38 @@ export default function Register() {
     <View style={styles.container}>
     <NativeBaseProvider>
         <VStack space={4} alignItems="center" mt="10%">
-        <Text bold fontSize="3xl" color="indigo.600">Register</Text>
+        <Text bold fontSize="3xl" color="#16a085" underline>Registration from</Text>
 
-        <Text top="40px" marginRight="62%" bold fontSize="sm" color="indigo.400">User Name</Text>
-        <Input  mt="6%" w="80%" variant="underlined" placeholder="Enter User Name" />
-        <Text top="20px" marginRight="71%" bold fontSize="sm" color="indigo.400">Email</Text>
-        <Input w="80%" variant="underlined" placeholder="Enter Email" />
-        <Text top="20px" marginRight="65%" bold fontSize="sm" color="indigo.400">Password</Text>
-        <Input w="80%" variant="underlined" placeholder="Enter Password" />
-        <Button mt="5%" w="50%" h="10%" backgroundColor="indigo.600" borderRadius="20" size="sm">Create account</Button>
+        <Text top="40px" marginRight="71%" bold fontSize="sm" color="#079992">User Name</Text>
+        <Input  mt="6%" w="85%" variant="underlined" placeholder="Enter User Name" />
+        <Text top="20px" marginRight="78%" bold fontSize="sm" color="#079992">Email</Text>
+        <Input w="85%" variant="underlined" placeholder="Enter Email" />
+        <Text top="20px" marginRight="72%" bold fontSize="sm" color="#079992">Password</Text>
+        <Input w="85%" variant="underlined" placeholder="Enter Password" />
+        <Button mt="5%" w="50%" h="10%" backgroundColor="#16a085" borderRadius="10" size="lg">Create account</Button>
         </VStack>
 
-        <Divider mt="10%" left="40px" w="80px" my="2" _light={{
+        <Divider mt="10%"  bottom="-3px"  left="20px" h="2px" w="80px" my="2" _light={{
         bg: "muted.800"
       }} _dark={{
         bg: "muted.50"
       }} />
-      <Text bottom="19px" left="150px"  fontSize="sm">Or continue with</Text>
+      <Text bottom="19px" left="127px"  fontSize="lg">Or continue with</Text>
 
-      <Divider bottom="35px" left="280px" w="80px" my="2" _light={{
+      <Divider bottom="37px" left="260px" h="2px" w="80px" my="2" _light={{
         bg: "muted.800"
       }} _dark={{
         bg: "muted.50"
       }} />
 
-<Image  left="90px" bottom="12px" size="50px"Thumbnail source= {require('./assets/img/google-logo.png')} alt="car logo" />
-            <Image  left="170px" bottom="59px" size="40px"Thumbnail source= {require('./assets/img/Facebook-logo.png')} alt="car logo" />
-            <Image  left="250px" bottom="103px" size="46px"Thumbnail source= {require('./assets/img/apple-logo.png')} alt="car logo" />
+        <Image  left="80px" bottom="12px" size="50px"Thumbnail source= {require('./assets/img/google-logo.png')} alt="car logo" />
+            <Image  left="162px" bottom="59px" size="40px"Thumbnail source= {require('./assets/img/Facebook-logo.png')} alt="car logo" />
+            <Image  left="235px" bottom="103px" size="46px"Thumbnail source= {require('./assets/img/apple-logo.png')} alt="car logo" />
 
-            <Text bottom="70px" left="80px" fontSize="xs">By creating an account, you agree to the </Text>
-            <Text bottom="70px" left="27px" color="indigo.600" fontSize="xs">VehicleInfo.com Free Membership Agreement </Text>
-            <Text bottom="88px" left="276px" fontSize="xs">and </Text>
-            <Text bottom="106px" left="201px" color="indigo.600" fontSize="xs">Privacy Policy </Text>
+            <Text bottom="80px" left="80px" fontSize="sm">By creating an account, you agree to the </Text>
+            <Text bottom="80px" left="40px" color="indigo.600" fontSize="xs">VehicleInfo.com Free Membership Agreement </Text>
+            <Text bottom="97px" left="235px" fontSize="sm">&</Text>
+            <Text bottom="112px" left="250px" color="indigo.600" fontSize="xs">Privacy Policy </Text>
             
     </NativeBaseProvider>
     </View>
