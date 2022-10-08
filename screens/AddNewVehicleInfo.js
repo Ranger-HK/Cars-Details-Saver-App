b844ed8e6903afe67b1a72ec5524066c5ead66f2
Ranger-HK <maxravindu99@gmail.com>
2022-10-08 20:05:34 +0530

    Backend Updated

--- screens/AddNewVehicleInfo.js
+++ screens/AddNewVehicleInfo.js
@@ -8,25 +8,26 @@ export default function AddNewVehicleInfo({navigation}) {
     <View style={styles.container}>
      <NativeBaseProvider>
      <VStack space={4} alignItems="center" mt="10%">
-     <Text bold fontSize="xl" color="indigo.600">Add New Vehicle Infomaion</Text>
+     <Text bold fontSize="3xl" bottom="15px" underline color="#16a085">Add New Vehicle Information</Text>
 
-     <Box mt="5%" height="20%" width="50%" alignSelf="center" borderWidth="2" borderColor="indigo.400" borderRadius="20" _text={{
+     <Box mt="5%" height="20%" bottom="15px" width="70%" alignSelf="center" borderWidth="3" borderColor="#485460" borderRadius="20" _text={{
       fontSize: "md",
       fontWeight: "medium",
       color: "warmGray.50",
       letterSpacing: "lg"
     }} icon={<Ionicons color="black" name={"duplicate-outline"} size={18}/>}>
-        This is a
       </Box>
     
-        <Text top="10px" left="-92" bold fontSize="sm" color="indigo.400">Vehicle Brand Name</Text>
-        <Input  bottom="2" w="80%" variant="underlined" placeholder="Vehicle Brand Name" />
-        <Text top="0px" left="-92" bold fontSize="sm" color="indigo.400">Registation Number</Text>
-        <Input  bottom="5" w="80%" variant="underlined" placeholder="Enter Registation Number" />
-        <Text top="-23px" left="-100" bold fontSize="sm" color="indigo.400">Other Infomation</Text>
-        <Input bottom="79" w="80%" h="20%" variant="underlined" placeholder="Enter Other Infomation" />
-        <Button mt="0%" w="50%" h="8%" backgroundColor="indigo.600" borderRadius="20" size="sm" onPress={()=>{navigation.navigate("Home")}}>Save</Button>
-
+        <Text top="30px" left="-98" bold fontSize="sm" color="#16a085">Vehicle Brand Name</Text>
+        <Input  bottom="-11" w="80%" variant="underlined" placeholder="Vehicle Brand Name" />
+        <Text top="20px" left="-98" bold fontSize="sm" color="#16a085">Registation Number</Text>
+        <Input  bottom="0" w="80%" variant="underlined" placeholder="Enter Registation Number" />
+        <Text top="10px" left="-104" bold fontSize="sm" color="#16a085">Other Infomation</Text>
+        <Input  bottom="2" w="80%" variant="underlined" placeholder="Enter Other Infomation" />
+        {/* <Text top="-15px" left="-103" bold fontSize="sm" color="#16a085">Other Infomation</Text>
+        <Input bottom="85" w="80%" h="20%" variant="underlined" placeholder="Enter Other Infomation" /> */}
+        <Button top="22px" mt="0%" w="50%" h="8%" backgroundColor="#16a085" borderRadius="10" size="lg" onPress={()=>{navigation.navigate("Home")}}>Save</Button>
+     
      </VStack>
       </NativeBaseProvider> 
       
